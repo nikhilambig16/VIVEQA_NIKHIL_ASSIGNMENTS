fb985a377ba7c41decf5e95a115cbdc406886fb2
nikhilambig16 <nikhil.ambig16@gmail.com>
2026-06-30 12:21:31 +0530

    Add files via upload

---
+++ day4/blocking_nonblocking.v
@@ -0,0 +1,28 @@
+module blk_assign();
+reg a,b,c,d;
+initial begin
+a=1'b1;
+b=1'b0;
+c=1'b1;
+d=1'b0;
+c=#5b;
+d=#10a;
+b=d;
+end
+endmodule
+
+
+module check_nonblock();
+reg a,b,c,d,i ,clk;
+initial begin
+a <= 1;
+b <= 1;
+c <= 1;
+d <= 1;
+i  <= 1;
+end
+initial begin
+i<=#51'b0;
+i<=#51'b1;
+end
+endmodule
